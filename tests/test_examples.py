68daba17a7c6b1f0c687dbc1c239e849e78fd875
Ryan Jarvis <ryan@technoloft.com>
2020-04-05 18:33:38 -0700

    Add tests for all original examples using code before this PR

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -1,66 +1,76 @@
-from patchbook import detailModule, exportJSON, graphviz, parseFile, printConnections, printDict
+import pytest
+
+import patchbook
+from patchbook import _PARSER_VERSION, detailModule, exportJSON, graphviz, parseFile, printConnections, printDict
 
 
+@pytest.mark.parametrize("patchname", ["patch1", "patch2", "syncpll"])
 class TestExamplePatch1:
-    fn = "../Examples/patch1.txt"
     maxDiff = None
 
-    def test_parsefile_raise_no_errors(self):
-        parseFile(self.fn)
+    def teardown_method(self, method):
+        """
+        Currently the code isn't idempotent so we need to do some cleanup after every test run.
 
-    def test_modules_prints_all_output(self, capsys):
+        Hopefully this teardown code can be removed soon
+        """
+        print("Resetting globals...")
+        patchbook.mainDict = {
+            "info": {"patchbook_version": _PARSER_VERSION},
+            "modules": {},
+            "comments": []
+        }
+        patchbook.lastModuleProcessed = ""
+        patchbook.lastVoiceProcessed = ""
+        patchbook.debugMode = False
+        patchbook.quiet = False
+        patchbook.connectionID = 0
+        patchbook.direction = ""
+
+    def test_parsefile_raise_no_errors(self, patchname):
+        parseFile(f"../Examples/{patchname}.txt")
+
+    def test_modules_prints_all_output(self, capsys, patchname):
         with capsys.disabled():
-            parseFile(self.fn)
+            parseFile(f"../Examples/{patchname}.txt")
 
         detailModule(all=True)
         captured = capsys.readouterr()
-        expected = open("sample_output/patch1_modules.txt").read()
+        expected = open(f"sample_output/{patchname}_modules.txt").read()
         assert expected == captured.out
 
-    def test_printDict_output(self, capsys):
+    def test_printDict_output(self, capsys, patchname):
         with capsys.disabled():
-            parseFile(self.fn)
+            parseFile(f"../Examples/{patchname}.txt")
 
         printDict()
         captured = capsys.readouterr()
-        expected = open("sample_output/patch1_print.txt").read()
+        expected = open(f"sample_output/{patchname}_print.txt").read()
         assert expected == captured.out
 
-    def test_exportJSON_output(self, capsys):
+    def test_exportJSON_output(self, capsys, patchname):
         with capsys.disabled():
-            parseFile(self.fn)
+            parseFile(f"../Examples/{patchname}.txt")
 
         exportJSON()
         captured = capsys.readouterr()
-        expected = open("sample_output/patch1_export.txt").read()
+        expected = open(f"sample_output/{patchname}_export.txt").read()
         assert expected == captured.out
 
-    def test_printConnections_output(self, capsys):
+    def test_printConnections_output(self, capsys, patchname):
         with capsys.disabled():
-            parseFile(self.fn)
+            parseFile(f"../Examples/{patchname}.txt")
 
         printConnections()
         captured = capsys.readouterr()
-        expected = open("sample_output/patch1_connections.txt").read()
+        expected = open(f"sample_output/{patchname}_connections.txt").read()
         assert expected == captured.out
 
-    def test_graphviz_output(self, capsys):
+    def test_graphviz_output(self, capsys, patchname):
         with capsys.disabled():
-            parseFile(self.fn)
+            parseFile(f"../Examples/{patchname}.txt")
 
         graphviz()
         captured = capsys.readouterr()
-        expected = open("sample_output/patch1_graph.txt").read()
+        expected = open(f"sample_output/{patchname}_graph.txt").read()
         assert expected == captured.out
-
-
-class TestExamplePatch2:
-    def test_parsefile_raise_no_errors(self):
-        fn = "../Examples/patch2.txt"
-        parseFile(fn)
-
-
-class TestExampleSyncPII:
-    def test_parsefile_raise_no_errors(self):
-        fn = "../Examples/syncpii.txt"
-        parseFile(fn)
